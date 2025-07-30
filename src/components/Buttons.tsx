775d853e2bd2f394efddb4618261fd71b0f2b45d
Hnsek <henrique2001sousa@gmail.com>
2025-07-30 17:17:07 -0300

    feat(button): create button to input files

---
+++ src/components/Buttons.tsx
@@ -0,0 +1,26 @@
+import { useRef, type ChangeEvent, type ReactNode } from "react"
+
+type FileButtonProps = {
+    text?:string,
+    onChange?: (event: ChangeEvent<HTMLInputElement>)  => void,
+    icon?: ReactNode,
+    acceptedFiles?:string[],
+    multiple?:boolean
+}
+
+export const FileButton = (props:FileButtonProps) => {
+    const inputRef = useRef<HTMLInputElement>(null)
+    return <>
+        <button className="flex items-center gap-1 bg-[var(--primary)] rounded text-[var(--text)] p-3 cursor-pointer hover:brightness-80" onClick={() =>  inputRef.current?.click()}>
+            <p>{props.text}</p>
+            {props.icon}
+        </button>
+        <input ref={inputRef} 
+            type="file" 
+            className="absolute hidden" 
+            onChange={(event) => props.onChange?.(event)} 
+            accept={props.acceptedFiles?.join(",")}
+            multiple={props.multiple}
+            />
+    </>
+}
