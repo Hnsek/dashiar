f40f7bb8810fff9b9f5955514964080c8db5be9b
Hnsek <henrique2001sousa@gmail.com>
2025-07-31 18:33:34 -0300

    chore(csv): adds service result type

--- src/services/csv.ts
+++ src/services/csv.ts
@@ -1,7 +1,8 @@
 
 import Papa from "papaparse"
 
-export const parse = (file: File) => {
+
+export const parse = (file: File) : Promise<Papa.ParseResult<unknown>> => {
     return new Promise((resolve, reject) => {
         Papa.parse(file, {
             complete:(data) => resolve(data),
