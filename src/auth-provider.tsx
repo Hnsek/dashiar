62b731ba94a1ccefa1d268b5e5e57469caba8699
Hnsek <henrique2001sousa@gmail.com>
2025-08-01 11:27:53 -0300

    feat(auth): creates provider

---
+++ src/auth-provider.tsx
@@ -0,0 +1,39 @@
+import { onAuthStateChanged, signOut, type User } from "firebase/auth"
+import { createContext, useContext, useEffect, useState, type ReactNode } from "react";
+import { auth } from "./firebase";
+
+
+export type AuthState = {
+    user: User | null;
+    isAuthenticated: boolean;
+    isLoading:boolean;
+    logout: () => Promise<void>
+}
+
+
+const AuthContext = createContext<AuthState | undefined>(undefined)
+
+export const AuthProvider = ({children } : {children : ReactNode}) => {
+    const [user, setUser] = useState<User|null>(null)
+    const [isLoading, setIsLoading] = useState<boolean>(true)
+
+    useEffect(() => {
+        onAuthStateChanged(auth,(user) => {
+            setUser(user)
+            setIsLoading(false)
+        })
+    }, [])
+
+    const logout = () => signOut(auth)
+
+    return <AuthContext.Provider value={{user, isLoading, isAuthenticated:!!user, logout}}>
+        {children}
+    </AuthContext.Provider>
+} 
+
+
+export function useAuth() {
+  const ctx = useContext(AuthContext);
+  if (!ctx) throw new Error('useAuth usado fora do AuthProvider');
+  return ctx;
+}
