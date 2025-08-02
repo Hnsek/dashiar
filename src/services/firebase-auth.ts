497bf4811ea10839f01010e59e8facebf6706749
Hnsek <henrique2001sousa@gmail.com>
2025-08-02 14:03:27 -0300

    feat(services): creates firebase auth service

--- src/services/firebase-auth.ts
+++ src/services/firebase-auth.ts
@@ -1,7 +1,16 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth"
+import { createUserWithEmailAndPassword, FacebookAuthProvider, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword as signInDefault } from "firebase/auth"
 import {auth} from "../firebase"
 
 export const signInWithGoogle = () => {
     const provider = new GoogleAuthProvider()
-    signInWithPopup(auth,provider)
+    return signInWithPopup(auth,provider)
 }
+
+export const signInWithFacebook = () => {
+    const provider = new FacebookAuthProvider()
+    return signInWithPopup(auth,provider)
+}
+
+export const signUp = (email : string, password: string) => createUserWithEmailAndPassword(auth, email, password)
+
+export const signInWithEmailAndPassword = (email: string, password:string) => signInDefault(auth,email, password)
