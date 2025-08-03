803e87485f4088ca00a1b8b5f5cf33417ca1a641
Hnsek <henrique2001sousa@gmail.com>
2025-08-03 10:39:44 -0300

    chore(route): removes logs

--- src/routes/(authenticated)/route.tsx
+++ src/routes/(authenticated)/route.tsx
@@ -3,8 +3,6 @@ import { createFileRoute, Outlet, redirect } from '@tanstack/react-router'
 export const Route = createFileRoute('/(authenticated)')({
   beforeLoad: ({ context, location }) => {
   
-      console.warn("context: ", context)
-  
       if (!context.auth.isAuthenticated) {
         throw redirect({
           to: '/login',
