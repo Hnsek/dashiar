3b4f6c80fcfe096147a3f8d7959a006918b184b9
Hnsek <henrique2001sousa@gmail.com>
2025-08-03 11:27:39 -0300

    feat(dashboard): implements data searching

--- src/routes/(authenticated)/dashboard.tsx
+++ src/routes/(authenticated)/dashboard.tsx
@@ -1,4 +1,9 @@
+import { DashboardSidebar } from '@/components/Sidebar';
+import { database } from '@/firebase';
+import { type Dashboard } from '@/types/data';
 import { createFileRoute } from '@tanstack/react-router'
+import { collection, onSnapshot, query } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
 
 type Search = {
   id:string | undefined;
@@ -15,7 +20,38 @@ function RouteComponent() {
 
   const { id } = Route.useSearch()
 
-  console.warn("id: ", id)
+  const [data, setData] = useState<Dashboard>()
 
-  return <main className='w-full h-screen bg-[var(--background)]'></main>
+  useEffect(() => {
+      const q = query(collection(database,"dashboards"))
+
+      onSnapshot(q, (snapshot) => {
+        const [data] : Dashboard[] = snapshot.docs.map((doc) => {
+          const dashboard  = {
+            id:doc.id,
+            ...doc.data()
+          } as Dashboard
+
+          return dashboard
+        })
+
+        console.warn("data: ", data)
+
+        setData({
+          ...data,
+          charts:[
+            {
+              height: 200,
+              width: 200,
+              y:15,
+              x:15
+            }
+          ]
+        })
+      })
+  },[])
+
+  return <main className='w-full h-screen bg-[var(--background)]'>
+      <DashboardSidebar/>
+  </main>
 }
