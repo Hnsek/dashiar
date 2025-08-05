998731d19b21cdc2a8498c11519404c8291c92c5
Hnsek <henrique2001sousa@gmail.com>
2025-08-05 12:45:35 -0300

    feat(types): creates chart types

--- src/types/data.ts
+++ src/types/data.ts
@@ -1,9 +1,10 @@
-import type { DataType } from "./data-type"
+import type { ChartFilterType, ChartOperation, ChartType, DataType } from "./data-type"
 
 export type DataValue = {
     name:string,
     values:unknown[]
 }
+
 
 export type Dataset = {
     name:string,
@@ -19,11 +20,29 @@ export type Dataset = {
 
 }
 
+export type ChartFilter = {
+    type: ChartFilterType,
+    value:number
+}
+
+export type ChartField = {
+    name: string,
+    type:ChartOperation,
+    key:string,
+    color:string,
+    value: unknown,
+    filters:ChartFilter[],
+    dataset:number[]
+}
+
+
 export type Chart = {
     x:number,
     y:number,
     width: number,
     height:number,
+    type:ChartType
+    fields:ChartField[],
 }
 
 export type Dashboard = {
