e91333674746e5021ca6539fca94e5c1a7e7056d
Hnsek <henrique2001sousa@gmail.com>
2025-08-05 18:39:03 -0300

    chore(home): changes home redirect

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -13,10 +13,10 @@ function App() {
           <h1 className='font-bold text-6xl text-center'>Create dashboards effortlessly!</h1>
           <p className='w-[90%] sm:w-[60%] text-center text-[var(--text-secondary)]'>Create powerful, customized dashboards in minutes—no technical skills or coding required. Our intuitive platform lets you visualize your data with easy drag-and-drop tools, so you can focus on insights instead of complexity.</p>
           <Link
-              to='/import-data'
+              to='/list'
               className='flex items-center gap-1 bg-[var(--primary)] rounded text-[var(--text)] p-3 cursor-pointer hover:brightness-80'
             >
-            <p>Import CSV</p>
+            <p>Dashboards</p>
             <FaArrowRight />
           </Link>
         </section>
