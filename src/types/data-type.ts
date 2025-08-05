fd88e5e3086a413c75edd29fc5abe7dd86213d56
Hnsek <henrique2001sousa@gmail.com>
2025-08-05 12:45:23 -0300

    feat(types): creates chart enums

--- src/types/data-type.ts
+++ src/types/data-type.ts
@@ -3,3 +3,21 @@ export enum DataType{
     NUMBER = "number",
     DATETIME = "datetime"
 } 
+
+export enum ChartType{
+    BAR="bar",
+    PIE="pie",
+    LINE="line"
+}
+
+export enum ChartOperation{
+    SUM="sum",
+    AVERAGE="average",
+    COUNT="count"
+}
+
+export enum ChartFilterType{
+    MORETHAN = "moreThan",
+    LESSTHAN = "lessThan",
+    EQUAL = "equal"
+}
