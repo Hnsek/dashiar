70c3f35ee1b7f61bf40c122613efcf85c0269bcc
Hnsek <henrique2001sousa@gmail.com>
2025-08-06 11:47:42 -0300

    feat(routes): creates main bar

--- src/routes/(authenticated)/route.tsx
+++ src/routes/(authenticated)/route.tsx
@@ -1,14 +1,36 @@
 import { createFileRoute, Outlet, redirect } from '@tanstack/react-router'
+import Logo from "../../assets/logo.svg"
+import { Menu, MenuButton, MenuItem } from '@szhsin/react-menu';
+import { FaUser } from "react-icons/fa";
+import { FiLogOut } from "react-icons/fi";
+import { logout } from '@/services/firebase-auth';
+import { isAuthenticated } from '@/auth-provider';
 
 export const Route = createFileRoute('/(authenticated)')({
   beforeLoad: ({ context, location }) => {
-  
-      if (!context.auth.isAuthenticated) {
+      if (!isAuthenticated) {
         throw redirect({
           to: '/login',
           search: { redirect: location.href },
         });
       }
     },
-    component: () => <Outlet />,
+    component: () => <div className='flex flex-col'>
+      <header className='bg-white h-18 border-b border-gray-300 flex justify-between p-4'>
+        <div className='h-full flex gap-2 flex-row items-center'>
+          <img src={Logo} className='h-full'/>
+          <p className='font-bold text-xl'>Dashiar</p>
+        </div>
+        <Menu
+          menuButton={<MenuButton className={"px-4 cursor-pointer w-15  h-full rounded-full bg-white hover:brightness-80 flex justify-center items-center"}><FaUser/></MenuButton>}>
+          <MenuItem 
+            onMouseDown={() => logout()}
+            className="bg-white px-5  flex gap-2 items-center p-2 rounded cursor-pointer border border-gray-300 hover:brightness-80">
+            <FiLogOut />
+            <p>Logout</p>
+          </MenuItem>
+        </Menu>
+      </header>
+      <Outlet />
+    </div>,
 })  
