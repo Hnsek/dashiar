6c40947ae701b891ce0d36cc1140009f70fa5ad4
Hnsek <henrique2001sousa@gmail.com>
2025-08-06 16:00:56 -0300

    feat(route): adds links to the main page

--- src/routes/(authenticated)/route.tsx
+++ src/routes/(authenticated)/route.tsx
@@ -1,10 +1,11 @@
-import { createFileRoute, Outlet, redirect } from '@tanstack/react-router'
+import { createFileRoute, Link, Outlet, redirect, useNavigate } from '@tanstack/react-router'
 import Logo from "../../assets/logo.svg"
 import { Menu, MenuButton, MenuItem } from '@szhsin/react-menu';
 import { FaUser } from "react-icons/fa";
 import { FiLogOut } from "react-icons/fi";
 import { logout } from '@/services/firebase-auth';
 import { isAuthenticated } from '@/auth-provider';
+import { FaChartPie } from "react-icons/fa";
 
 export const Route = createFileRoute('/(authenticated)')({
   beforeLoad: ({ location }) => {
@@ -15,14 +16,21 @@ export const Route = createFileRoute('/(authenticated)')({
         });
       }
     },
-    component: () => <div className='flex flex-col'>
-      <header className='bg-white h-18 border-b border-gray-300 flex justify-between p-4'>
-        <div className='h-full flex gap-1 flex-row items-center'>
-          <img src={Logo} className='h-full'/>
-          <p className='font-bold text-xl'>Dashiar</p>
-        </div>
+    component: () => <div className='flex flex-col h-screen overflow-hidden'>
+      <header className='bg-white h-18 border-b border-gray-300 flex justify-between p-4 z-100'>
+        <Link to='/' className='h-full flex gap-1 flex-row items-center'>
+            <img src={Logo} className='h-full'/>
+            <p className='font-bold text-xl'>Dashiar</p>
+        </Link>
         <Menu
           menuButton={<MenuButton className={"px-4 cursor-pointer w-15  h-full rounded-full bg-white hover:brightness-80 flex justify-center items-center"}><FaUser/></MenuButton>}>
+          <MenuItem 
+            className="bg-white px-5  flex gap-2 items-center p-2 rounded cursor-pointer border border-gray-300 hover:brightness-80 w-50">
+            <Link to='/list' className='flex gap-2 items-center'>
+              <FaChartPie />
+              <p>My dashboards</p>
+            </Link>
+          </MenuItem>
           <MenuItem 
             onMouseDown={() => logout()}
             className="bg-white px-5  flex gap-2 items-center p-2 rounded cursor-pointer border border-gray-300 hover:brightness-80">
