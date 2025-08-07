b2e3473d1902ea2242224dcecd13492ad1265812
Hnsek <henrique2001sousa@gmail.com>
2025-08-07 10:00:37 -0300

    feat(providers): creates modal provider

---
+++ src/utils/providers/modal.tsx
@@ -0,0 +1,39 @@
+import {createContext, useContext, useState, type ReactNode} from "react"
+
+
+const GlobalContext = createContext<{
+    show:(componente : ReactNode) => void,
+    hide: () => void
+}>({
+    show() {},
+    hide() {},
+})
+
+export const ModalProvider = ({children} : {children : ReactNode}) => {
+    const [component, setComponent] = useState<ReactNode>()
+    
+    const show = (component : ReactNode) => {
+        setComponent(() => component)
+    }
+    const hide = () => setComponent(undefined)
+
+    return <GlobalContext.Provider value={{ show, hide }}>
+        {children}
+        {component ? 
+            <div className="z-99999 w-full h-screen fixed top-0 left-0 flex items-center justify-center">
+                <div className="w-full h-screen bg-black opacity-80  absolute"></div>
+                <button 
+                    onMouseDown={() => hide()} 
+                    className="w-full h-full flex items-center justify-center z-50">
+                    {component}
+                </button>
+
+
+            </div>
+            :
+            undefined
+        }
+    </GlobalContext.Provider>
+} 
+
+export const useModal = () => useContext(GlobalContext)
