3196f80fb2b68d3cad69eb3805643e8ad1a47f36
Hnsek <henrique2001sousa@gmail.com>
2025-08-07 13:28:09 -0300

    refactor(providers): implements stripe provider

--- src/utils/providers/app-providers.tsx
+++ src/utils/providers/app-providers.tsx
@@ -2,7 +2,13 @@ import type { ComponentType, ReactNode } from "react";
 import { AuthProvider } from "./auth-provider";
 import { ModalProvider } from "./modal";
 
+import {
+  Elements,
+} from '@stripe/react-stripe-js';
+import { getStripe } from "@/config/stripe";
+
 const composeProviders = (...providers : ComponentType<{ children: ReactNode }>[]) => ({children} : {children:ReactNode}) => {
+    console.warn("children: ", children)
     return providers.reduceRight((accumulator, Provider) => {
         return <Provider>{accumulator}</Provider>
     },children)
@@ -10,6 +16,11 @@ const composeProviders = (...providers : ComponentType<{ children: ReactNode }>[
 
 
 export const AppProviders = composeProviders(
+    ({children}:{children : ReactNode}) => {
+        return <Elements stripe={getStripe()}>
+            {children}
+        </Elements>
+    },
     AuthProvider,
-    ModalProvider
+    ModalProvider,
 )
