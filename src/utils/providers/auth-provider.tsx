77551af76f892c243d0406c28f7441be3a8d4b5f
Hnsek <henrique2001sousa@gmail.com>
2025-08-07 16:45:42 -0300

    feat(auth-providers): adds subscriptions

--- src/utils/providers/auth-provider.tsx
+++ src/utils/providers/auth-provider.tsx
@@ -1,13 +1,16 @@
 import { onAuthStateChanged, signOut, type User } from "firebase/auth"
 import { createContext, useContext, useEffect, useState, type ReactNode } from "react";
-import { auth } from "../../config/firebase";
+import { auth, database } from "../../config/firebase";
+import { collection, onSnapshot, query, where } from "firebase/firestore";
+import type { Subscription } from "@/types/subscription";
 
 
 export type AuthState = {
     user: User | null;
     isAuthenticated: boolean;
     isLoading:boolean;
-    logout: () => Promise<void>
+    logout: () => Promise<void>,
+    subscriptions: Subscription[]
 }
 
 
@@ -17,20 +20,49 @@ const AuthContext = createContext<AuthState | undefined>(undefined)
 export const AuthProvider = ({children } : {children : ReactNode}) => {
     const [user, setUser] = useState<User|null>(null)
     const [isLoading, setIsLoading] = useState<boolean>(true)
+    const [subscriptions, setSubscriptions] = useState<Subscription[]>([])
 
     useEffect(() => {
-        onAuthStateChanged(auth,(user) => {
+        onAuthStateChanged(auth,async (user) => {
 
             isAuthenticated = !!user
 
             setUser(user)
             setIsLoading(false)
         })
+
+
+
+
     }, [])
+
+    useEffect(() => {
+
+        if(!user) {
+            return
+        }
+
+        const execution = async () => {
+            const subscriptionQuery = query(collection(database,"subscriptions"),where("userId","==", user?.uid))
+            
+            onSnapshot(subscriptionQuery, (result) => {
+                const subscriptions = result.docs.map((doc) => ({
+                        id:doc.id,
+                        ...doc.data()
+                    }) as Subscription)
+
+                    setSubscriptions(subscriptions)
+            })
+
+        }
+
+        execution()
+
+    }, [user])
 
     const logout = () => signOut(auth)
 
-    return <AuthContext.Provider value={{user, isLoading, isAuthenticated:!!user, logout}}>
+    return <AuthContext.Provider value={{user, isLoading, isAuthenticated:!!user, logout, subscriptions}}>
         {children}
     </AuthContext.Provider>
 } 
