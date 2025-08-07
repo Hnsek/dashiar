b13b7055705b038f42787c695b82c61721ea3ccd
Hnsek <henrique2001sousa@gmail.com>
2025-08-07 13:29:11 -0300

    fix(firebase): changes values to env

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore"
 import { getAuth } from "firebase/auth"
 
 const config = {
-  apiKey: "AIzaSyBDRb1ULOEOrZmjfLqcJUlUJjRk_0tkkYs",
-  authDomain: "dashiar-ea721.firebaseapp.com",
-  projectId: "dashiar-ea721",
-  storageBucket: "dashiar-ea721.firebasestorage.app",
-  messagingSenderId: "935586107976",
-  appId: "1:935586107976:web:c1162d33c6cca765ef3e7d"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 }
 
 export const app = initializeApp(config)
